8fe5154f547c6a3606e918f43a2c541295835c98
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-02-13 17:56:42 +0530

    Add Sum.java with commented code for summing two integers

---
+++ 01_Main.java/Sum.java
@@ -0,0 +1,13 @@
+// import java.util.*;
+
+// public class Sum {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         int num1 = input.nextInt();
+//         int num2 = input.nextInt();
+
+//         int Sum = num1 + num2;
+
+//         System.out.println("Sum ="+ Sum);
+//     }
+// }
