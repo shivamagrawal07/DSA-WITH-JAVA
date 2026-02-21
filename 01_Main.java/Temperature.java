6590b605c5d66fb83ae1c087b4a26854a1237ea2
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-02-21 19:23:30 +0530

    temparature

---
+++ 01_Main.java/Temperature.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Temperature {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter a temp in celcius:");
+        float tempcelcius = sc.nextFloat(); 
+
+        float tempF = (tempcelcius * 9/5) + 32;
+
+        System.out.println(tempF);
+    }
+}
