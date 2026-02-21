d109f436fc050ef70ece3a482afe1d9a29ef8a4e
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-02-21 19:23:42 +0530

    typecasting

--- 01_Main.java/Typecasting.java
+++ 01_Main.java/Typecasting.java
@@ -1,9 +1,39 @@
-import java.util.*;
-
+ 
 public class Typecasting {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Float num = sc.nextFloat();
-        System.out.println(num);
+        // Scanner sc = new Scanner(System.in);
+        // Float num = sc.nextFloat();
+        // System.out.println(num);
+
+        //typecasting
+        // int newNum = (int)(67.56f);
+        // System.out.println(newNum);
+
+        // automatic type promotion in expression
+        // int a = 257;
+        // byte b = (byte)(a);
+
+        // byte a = 40;
+        // byte b = 50;
+        // byte c = 100;
+        // int d = (a * b) / c;
+        
+        // System.out.println(d);
+
+        // int number = 'A';
+        // System.out.println(number);
+
+        // System.out.println(3*6);
+
+        byte b = 42;
+        char c = 'a';
+        short s = 1024;
+        int i = 50000;
+        float f = 5.67f;
+        double d = 0.1234;
+        double result = (f * b) + (i/ c) - (d - s);
+        System.out.println((f * b) + " " + (i/ c) + " " + (d - s));
+        System.out.println(result);
     }
+
 }
