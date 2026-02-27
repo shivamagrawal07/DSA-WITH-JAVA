2d8acacd5b759add7541ecf48429e3229e3ffddf
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-02-28 02:25:51 +0530

    java

---
+++ 03_function-methods.java/sum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class sum {
+    public static void main(String[] args) {
+            sum();
+    }
+
+    static void sum(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number 1:");
+        int num1 = in.nextInt();
+        System.out.println("Enter number 2:");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+
+
+    }
+
+    /*
+        return type name (){
+        //body
+        return statement;
+        }  
+    */
+}
