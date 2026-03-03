0d51b8ca5238c6e0d28b5079b9679fe879671cdf
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-03-04 01:09:22 +0530

    java

--- 03_function-methods.java/shadowing.java
+++ 03_function-methods.java/shadowing.java
@@ -1,3 +1,16 @@
+
 public class shadowing {
-    
+    static int x = 90;
+    public static void main(String[] args) {
+        System.out.println(x); //90
+        fun();
+        int x; // the class variable at line 4 is shadowed by this
+
+        x = 40;
+        System.out.println(x); //40
+    }
+
+    static void fun(){
+        System.out.println(x);
+    }
 }
