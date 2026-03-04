69d96adc1d55368ade313988f77cc60a5327b11d
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-03-04 23:28:31 +0530

    java

---
+++ 03_function-methods.java/varArgs.java
@@ -0,0 +1,12 @@
+// import java.util.Arrays;
+
+// public class varArgs {
+//     public static void main(String[] args) {
+//         fun(...v: 2, 3, 4, 5,56, 87, 23, 45, 65); 
+//    }
+
+//    static void fun(int ....v){
+//     System.out.println(Arrays.toString(v));
+//    }
+
+// }
