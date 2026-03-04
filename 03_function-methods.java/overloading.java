52c6ccd71562cf315a4c2c537104db3a478dbe5d
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-03-04 23:35:40 +0530

    java

--- 03_function-methods.java/overloading.java
+++ 03_function-methods.java/overloading.java
@@ -1,8 +1,18 @@
 
 public class overloading {
     public static void main(String[] args) {
-        fun(67);
-        fun ("shivam agrawal");
+        // fun(67);
+        // fun ("shivam agrawal");
+       int ans = sum (3, 4, 78);
+       System.out.println(ans);
+    }
+
+    static int sum(int a , int b) {
+        return a + b;  
+    } 
+
+    static int sum(int a , int b, int c){
+        return a + b + c;
     }
 
     static void fun(int a){
