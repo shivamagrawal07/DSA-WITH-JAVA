a495add41c3dcb49d4b813559491f799291cc843
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-03-05 12:45:02 +0530

    java

--- 03_function-methods.java/Questions.java
+++ 03_function-methods.java/Questions.java
@@ -2,10 +2,45 @@ import java.util.*;
 
 public class Questions {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+        // int n = in.nextInt();
+        // boolean ans = isPrime(n);
+        // System.out.println(ans);
 
-        // Your code here
+        for (int i = 100; i < 1000; i++) {
+            if(isArmstrong(i)){
+                System.out.print(i + " ");
+            }
+        }
 
-        sc.close();
     }
+
+    // print all the 3 digit armstrong number
+    static boolean isArmstrong(int n){
+        int original = n;
+        int sum = 0;
+
+        while (n > 0){
+            int rem = n % 10;
+            n = n / 10; 
+            sum = sum + rem*rem*rem;
+        }
+
+        return sum == original;
+    } 
+
+
+    static boolean isPrime(int n) {
+        if (n <= 1 ) {
+            return false;
+        }
+        int c = 2;
+        while (c * c <= n) {
+            if (n % c == 0){
+                return false;
+            }
+            c++;
+        }
+        return true;
+   }
 }
