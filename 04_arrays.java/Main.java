6c9c2177a336e679850350b52ee1fe595d4b22af
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-03-05 16:45:45 +0530

    java

---
+++ 04_arrays.java/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //Q: store a roll no
+        int a = 19;
+            
+        //Q: store a person's name
+        String name = "Shivam Agrawal";
+
+        //Q: Store 5 roll numbers
+        int rno1 = 23;    
+        int rno2 = 55;    
+        int rno3 = 18;
+        
+        //Syntax
+        //dataype [] variable_name = new datatype[size];
+        // //store 5 roll number:
+        // int[] rnos = new int [5];
+        // // or directly
+        // int [] rnos2 = {23, 12, 45, 32, 15};
+
+        int [] ros; //declaration of array. ros is getting defined in the stack 
+        ros = new int[5]; //actually here object is being created in the memory (heap)\
+        
+        // System.out.println(ros[1])
+
+        // String[] arr = new String[4];
+        // System.out.println(arr[0]); 
+
+
+    }
+}
