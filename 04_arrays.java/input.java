ba8e01488cde3791240652f59f7cd980ccc3b49d
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-03-05 17:06:43 +0530

    java

---
+++ 04_arrays.java/input.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5];
+        arr[0] = 23;
+        arr[1] = 45;
+        arr[2] = 233;
+        arr[3] = 543;
+        arr[4] = 3;
+        // System.out.println(arr[3]);
+
+        // input using for loops
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println(Arrays.toString(arr));
+        
+        //  for (int i = 0; i < arr.length; i++) {
+        //     System.out.println(arr[i] + " ");
+        // }
+
+        for(int num : arr){     //for every element in the array, print the element 
+            System.out.println(num + " "); // here num represent element of the array
+        }
+         // System.out.println(arr[5]); //index out of bound error 
+    }
+}
+
