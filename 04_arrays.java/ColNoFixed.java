5c190e5ab273297cdba7af496e32a66d755b538b
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-03-08 10:20:47 +0530

    java

---
+++ 04_arrays.java/ColNoFixed.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class ColNoFixed {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [][] arr = {
+            {1, 2, 3, 4},
+            {5, 6},
+            {7, 8, 9}
+        };
+
+        for (int row = 0; row < arr.length; row++ ){
+            for (int col = 0; col <arr[row].length; col++) {
+                System.out.print(arr[row] [col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
