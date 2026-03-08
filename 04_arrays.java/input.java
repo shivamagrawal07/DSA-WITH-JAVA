72656141d428adc82dcbe9f08fb2471166f6cab9
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-03-08 10:20:57 +0530

    java

--- 04_arrays.java/input.java
+++ 04_arrays.java/input.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class input {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        
+        //array of primitives
         int[] arr = new int[5];
         arr[0] = 23;
         arr[1] = 45;
@@ -12,20 +14,35 @@ public class input {
         // System.out.println(arr[3]);
 
         // input using for loops
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = in.nextInt();
-        }
+        // for (int i = 0; i < arr.length; i++) {
+        //     arr[i] = in.nextInt();
+        // }
 
-        System.out.println(Arrays.toString(arr));
+        // System.out.println(Arrays.toString(arr));
         
         //  for (int i = 0; i < arr.length; i++) {
         //     System.out.println(arr[i] + " ");
         // }
 
         for(int num : arr){     //for every element in the array, print the element 
-            System.out.println(num + " "); // here num represent element of the array
+            // System.out.println(num + " "); // here num represent element of the array
         }
          // System.out.println(arr[5]); //index out of bound error 
+
+         //array of objects
+         String[] str = new String[4];
+         for (int i = 0; i < str.length; i++) {
+            str[i] = in.next();
+             
+         }
+
+         System.out.println(Arrays.toString(str));
+
+         //modify
+         str[1] = "shivam";
+
+         System.out.println(Arrays.toString(str));
+
     }
 }
 
