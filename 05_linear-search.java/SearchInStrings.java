b1e0f28f7d7ca127d35ad9b0a625a6d7115c8238
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-03-16 23:59:06 +0530

    java

---
+++ 05_linear-search.java/SearchInStrings.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class SearchInStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            String name = "shivam";
+            char target = 'u';
+            // System.out.println(search(name, target));
+
+            System.out.println(Arrays.toString(name.toCharArray()));
+    }
+
+     static boolean search2(String str, char target){
+            if(str.length() == 0){          // this is a function 
+                return false;
+            }
+              
+        for (char ch: str.toCharArray()){
+            if(ch == target){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+        static boolean search(String str, char target){
+            if(str.length() == 0){          // this is a function 
+                return false;
+            }
+              
+        for (int i = 0; i < str.length(); i++) {
+            if(target == str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
