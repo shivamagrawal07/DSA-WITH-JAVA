42e160c414f0c2516ba64c250daeadc1f56c081c
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-03-16 23:58:42 +0530

    java

---
+++ 05_linear-search.java/SearchInRange.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class SearchInRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Your code here
+
+        sc.close();
+    }
+}
