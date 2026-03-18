5d420867c3670b36c2031150fd9c95b9fee1919d
shivamagrawal07 <shivamagrawal041@gmail.com>
2026-03-18 09:04:10 +0530

    java

---
+++ 05_linear_search.java/SearchInRange.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class SearchInRange {
+    public static void main(String[] args) {
+        int [] arr = {18, 12, -7, 3, 14, 28};
+        int target = 14;
+        System.out.println(linearSearch(arr, target, 1, 4));
+        Scanner sc = new Scanner(System.in);
+
+        
+    }
+
+      static int linearSearch(int[] arr, int target, int start, int end){
+        if(arr.length == 0 ){
+            return-1;
+        } 
+
+        //run for a loop
+        for (int index = start; index <= end; index++) {
+            //check for element at every index if it is = target
+            int element = arr[index];
+            if (element == target){
+                return index;
+            }
+        }
+        //this line will execute if none of the return statements above have executed 
+        //hence the target not found
+        return -1;
+    }
+}
